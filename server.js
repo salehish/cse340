7c5dcea20802f6359a8df30f92fa9ab3fc55ec4e
salehish <ntegesaleh@gmail.com>
2025-06-19 23:12:32 +0300

    change E

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-/* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
 /* ***********************
  * Require Statements
  *************************/
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
+require("dotenv").config()
 const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/basaController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const accountRoute = require("./routes/accountRoute")
-const utilities = require("./utilitiies/")
 const session = require("express-session")
 const pool = require('./database/')
 const bodyParser = require("body-parser")
-const routeIndex = require("./routes/")
 const cookieParser = require("cookie-parser")
+
+// Routes and utilities
+const staticRoutes = require("./routes/static")
+const baseController = require("./controllers/basaController")
+const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
+const routeIndex = require("./routes/")
+const utilities = require("./utilitiies/")
 
 /* ***********************
  * Middleware
- * ************************/
+ *************************/
 app.use(session({
   store: new (require('connect-pg-simple')(session))({
     createTableIfMissing: true,
@@ -32,82 +30,73 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   name: 'sessionId',
+  cookie: {
+    secure: process.env.NODE_ENV === 'production', // Only secure cookies in production
+    maxAge: 1000 * 60 * 60 * 2 // 2 hours
+  }
 }))
 
-
+// Session data to views
 app.use((req, res, next) => {
-  res.locals.loggedin = req.session.loggedin || false;
-  res.locals.accountData = req.session.accountData || null;
-  next();
-});
+  res.locals.loggedin = req.session.loggedin || false
+  res.locals.accountData = req.session.accountData || null
+  next()
+})
 
-
-// Express Messages Middleware
+// Flash messages
 app.use(require('connect-flash')())
 app.use(function(req, res, next){
   res.locals.messages = require('express-messages')(req, res)
   next()
 })
 
+// Other middlewares
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(utilities.checkJWTToken)
 
 /* ***********************
- * View engine and Templates
+ * View Engine
  *************************/
-
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("layout", "./layouts/layout")
+
 /* ***********************
  * Routes
  *************************/
-app.use(static)
-// Index route
+app.use(staticRoutes)
 app.use("/", routeIndex)
-// Inventory routes
 app.use("/inv", inventoryRoute)
-
-//Error route.
-app.get("/trigger-error", utilities.handleErrors(baseController.errorLink))
-
-//account routes
 app.use("/account", accountRoute)
 
-// File Not Found Route - must be last route in list
+// Error testing route
+app.get("/trigger-error", utilities.handleErrors(baseController.errorLink))
+
+// 404 handler (keep last)
 app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+  next({ status: 404, message: 'Sorry, we appear to have lost that page.' })
 })
 
 /* ***********************
- * Local Server Information
- * Values from .env (environment) file
+ * Global Error Handler
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
-
-
-/* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
-  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
-  res.render("errors/error", {
-    title: err.status || 'Server Error',
+  const message = err.status === 404 ? err.message : "Oh no! There was a crash. Maybe try a different route?"
+  res.status(err.status || 500).render("errors/error", {
+    title: err.status || "Server Error",
     message,
     nav
   })
 })
 
 /* ***********************
- * Log statement to confirm server operation
+ * Start Server
  *************************/
-
+const port = process.env.PORT || 5500
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
